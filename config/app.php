34a0aa8b5e3a67c28d66e8521e6bfb1baf488da8
Oscar <osel0810029@gmail.com>
2017-09-30 19:47:38 -0500

    Paquetes
    Se agregaron paquetes al proyecto, uno de ellos servira para los sluggs y el otro es para los formularios, que permitira crear objetos del formulario sin necesidad de introducir codigo html

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Providers personales
+         */
+        App\Providers\AnnotationsServiceProvider::class,
+
     ],
 
     /*
--- database/migrations/2017_09_23_010336_add_articles_table.php
+++ database/migrations/2017_09_23_010336_add_articles_table.php
@@ -19,6 +19,7 @@ class AddArticlesTable extends Migration
             $table->text('content');
             $table->integer('user_id')->unsigned();
             $table->integer('category_id')->unsigned();
+            $table->string('slug')->nullable();
 
             $table->foreign('user_id')->references('id')->on('users')->ondelete('cascade');
             $table->foreign('category_id')->references('id')->on('categories')->ondelete('cascade');
